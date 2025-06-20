042d355607ed6d2c9be2a4537daf38861451b3cf
haskex <jhuan2024@hotmail.com>
2025-06-20 14:25:28 -0300

    Changing Books names

--- homes/haskex/default.nix
+++ homes/haskex/default.nix
@@ -8,6 +8,12 @@
     homeDirectory = "/home/haskex";
     stateVersion = "25.11";
 
+    file = {
+      "Lisp/Practical Common Lisp.pdf".source = ../../assets/Books/Practical_Common_Lisp.pdf;
+
+      "Haskell/Learn You a Haskell for Great Good".source = ../../assets/Books/Learn_You_a_Haskell_for_Great_Good.pdf;
+    };
+
     packages = with pkgs; [
       mpv
       cava
